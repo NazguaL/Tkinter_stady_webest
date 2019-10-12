fee754e5ffa9bf6094697f516f9f6be7e4036aef
nazgual <nazgual@ukr.net>
2019-10-12 22:25:09 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=3sf2MRgLNN0&list=PLfAlku7WMht4Vm6ewLgdP9Ou8SCk4Zhar
+from tkinter import *
+
+root = Tk()
+
+the_label = Label(root, text="Библиотека Tkinter - 1 - Установка tkinter")
+the_label.pack()
+
+root.mainloop()
